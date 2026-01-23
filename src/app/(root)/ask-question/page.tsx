cfdbc9a2faa3091819b200bf83ad814e884781c1
Himanshu Sagar <pixellateddev@proton.me>
2026-01-24 00:35:37 +0530

    Added question form

--- src/app/(root)/ask-question/page.tsx
+++ src/app/(root)/ask-question/page.tsx
@@ -1,7 +1,16 @@
 import type { FC } from 'react'
+import QuestionForm from '@/components/forms/question-form'
 
 const AskQuestion: FC = () => {
-  return null
+  return (
+    <>
+      <h1 className='h1-bold text-dark100_light900'>Ask a question</h1>
+
+      <div className='mt-9'>
+        <QuestionForm />
+      </div>
+    </>
+  )
 }
 
 export default AskQuestion
