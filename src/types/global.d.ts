37b94a3fb737aee177f865fbcd928eecf0f338a5
Himanshu Sagar <pixellateddev@proton.me>
2026-01-24 14:21:25 +0530

    added user routes

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -20,3 +20,19 @@ interface Author {
   name: string
   image: string
 }
+
+type ActionResponse<T = null> = {
+  success: boolean
+  data?: T
+  error?: {
+    message: string
+    details?: Record<string, string[]>
+  }
+  status?: number
+}
+
+type SuccessResponse<T = null> = ActionResponse<T> & { success: true }
+type ErrorResponse = ActionResponse<undefined> & { success: false }
+
+type APIErrorResponse = NextResponse<ErrorResponse>
+type APIResponse<T = null> = NextResponse<SuccessResponse<T> | ErrorResponse>
